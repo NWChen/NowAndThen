de29cf12c40dfdf4bd4314dab16c368b75dab7bf
NWChen <neilwchen@gmail.com>
2015-07-19 12:48:28 -0400

    meteor version

---
+++ han-meteor.js
@@ -0,0 +1,86 @@
+Events = new Mongo.Collection("events");
+
+marker = ["40.749704", "-73.969175", "UNITED NATIONS HEADQUARTERS BUILT", "1952", "The United Nations was formulated among delegations from the Soviet Union, the US, the UK, and China. The United Nations Charter was officially ratified by the five permanent members of the council - the four aforementioned, and France. 46 other signatories were present. In 1945, the construction of the United Nations Headquarters in New York City was finally completed."];
+maxDistance = 70;
+
+geocoder = null;
+map = null;
+panorama = null;
+
+if (Meteor.isClient) {
+  Meteor.startup(function(){
+
+    function initialize() {
+      geocoder = new google.maps.Geocoder();
+      var fenway = new google.maps.LatLng(42.345573, -71.098326);
+      var mapOptions = {
+        center: fenway,
+        zoom: 14
+      };
+      map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+      var panoramaOptions = {
+        position: fenway,
+        pov: {
+          heading: 34,
+          pitch: 10
+        }
+      };
+      panorama = new google.maps.StreetViewPanorama(document.getElementById('sv'), panoramaOptions);
+      map.setStreetView(panorama);
+    };
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+  });
+
+  Template.main.events({
+    'click #go': function(e, t) {
+      var address = document.getElementById('location').value;
+      geocoder.geocode({'address': address}, function(results, status){
+        if(status == google.maps.GeocoderStatus.OK){
+          map.setCenter(results[0].geometry.location);
+          panorama.setPosition(results[0].geometry.location);
+          var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+          });
+        } else {
+          alert("Failed geocode.");
+        }
+      });
+    },
+
+    'click': function(e, t) {
+      var l_marker = new google.maps.LatLng(marker[0], marker[1]);
+      var distance = google.maps.geometry.spherical.computeDistanceBetween(l_marker, panorama.getPosition());
+      console.log(distance, maxDistance);
+      if(distance<maxDistance) {
+        Session.set("inRange", "inRange");
+        Session.set("title", marker[2]);
+        Session.set("date", marker[3]);
+        Session.set("description", marker[4]);
+      } else {
+        Session.set("inRange", "");
+        Session.set("title", "");
+        Session.set("date", "");
+        Session.set("description", "");
+      }
+    }
+  });
+
+  Template.main.helpers({
+    inRange: function() {
+      return Session.get("inRange");
+    },
+    title: function(){
+      return Session.get("title");
+    },
+    date: function(){
+      return Session.get("date");
+    },
+    description: function(){
+      return Session.get("description");
+    }
+  })
+
+}

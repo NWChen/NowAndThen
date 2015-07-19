6ded063569fb28c300577866ca10a12d5bbe67d3
NWChen <neilwchen@gmail.com>
2015-07-19 13:02:58 -0400

    multiple markers

--- han-meteor.js
+++ han-meteor.js
@@ -1,6 +1,11 @@
 Events = new Mongo.Collection("events");
 
-marker = ["40.749704", "-73.969175", "UNITED NATIONS HEADQUARTERS BUILT", "1952", "The United Nations was formulated among delegations from the Soviet Union, the US, the UK, and China. The United Nations Charter was officially ratified by the five permanent members of the council - the four aforementioned, and France. 46 other signatories were present. In 1945, the construction of the United Nations Headquarters in New York City was finally completed."];
+markers = [
+  marker1 = ["40.749704", "-73.969175", "UNITED NATIONS HEADQUARTERS BUILT", "1952", "The United Nations was formulated among delegations from the Soviet Union, the US, the UK, and China. The United Nations Charter was officially ratified by the five permanent members of the council - the four aforementioned, and France. 46 other signatories were present. In 1945, the construction of the United Nations Headquarters in New York City was finally completed."],
+  marker2 = ["40.749704", "-73.969175", "UNITED NATIONS HEADQUARTERS BUILT", "1952", "The United Nations was formulated among delegations from the Soviet Union, the US, the UK, and China. The United Nations Charter was officially ratified by the five permanent members of the council - the four aforementioned, and France. 46 other signatories were present. In 1945, the construction of the United Nations Headquarters in New York City was finally completed."],
+  marker3 = ["40.749704", "-73.969175", "UNITED NATIONS HEADQUARTERS BUILT", "1952", "The United Nations was formulated among delegations from the Soviet Union, the US, the UK, and China. The United Nations Charter was officially ratified by the five permanent members of the council - the four aforementioned, and France. 46 other signatories were present. In 1945, the construction of the United Nations Headquarters in New York City was finally completed."]
+];
+
 maxDistance = 70;
 
 geocoder = null;
@@ -51,18 +56,21 @@ if (Meteor.isClient) {
     },
 
     'click': function(e, t) {
-      var l_marker = new google.maps.LatLng(marker[0], marker[1]);
-      var distance = google.maps.geometry.spherical.computeDistanceBetween(l_marker, panorama.getPosition());
-      if(distance<maxDistance) {
-        Session.set("inRange", "inRange");
-        Session.set("title", marker[2]);
-        Session.set("date", marker[3]);
-        Session.set("description", marker[4]);
-      } else {
-        Session.set("inRange", "");
-        Session.set("title", "");
-        Session.set("date", "");
-        Session.set("description", "");
+      for(var i=0; i<markers.length; i++) {
+        var l_marker = new google.maps.LatLng(markers[i][0], markers[i][1]);
+        var distance = google.maps.geometry.spherical.computeDistanceBetween(l_marker, panorama.getPosition());
+        console.log(distance);
+        if(distance<maxDistance) {
+          Session.set("inRange", "inRange");
+          Session.set("title", markers[i][2]);
+          Session.set("date", markers[i][3]);
+          Session.set("description", markers[i][4]);
+        } else {
+          Session.set("inRange", "");
+          Session.set("title", "");
+          Session.set("date", "");
+          Session.set("description", "");
+        }
       }
     }
   });
